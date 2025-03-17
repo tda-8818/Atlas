a2b9e8551304f6cc05104750c33d22e00f5901ab
SweeJieNg <ngsweejie@gmail.com>
2025-03-17 14:03:38 +1100

    setup models folder
    do more research on how to use mongoose and schema to actually access the db

---
+++ server/src/models/user.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+// TODO: LEARN MONGOOSE SCHEMA 
+
+
+// const userSchema = new mongoose.Schema(
+
+//     {
+//         userID: {
+//             type:String,
+//             required: true
+//         }
+
+//     }
+// )
