c8bdb5dfbbb13bd7816a90a3445f07ad7dcbbad2
SweeJieNg <ngsweejie@gmail.com>
2025-03-19 11:47:36 +1100

    added files to for user-db testing
    also created listCollections to print the names of the collections in a given db

--- server/server_db.js
+++ server/server_db.js
@@ -30,6 +30,28 @@ async function dropCollection(client, dbName, collectionName) {
     }
 }
 
+/**
+ * Prints a list of all collections within a given database to the console.
+ *
+ * @param {MongoClient} client - The Mongoclient connected to the DB.
+ * @param {string} dbName - The name of the DB you want to access.
+ * @returns nothing
+ * @throws {NotFoundError} - If no user is found with the given IDs.
+ * @throws {Error} - If there is an error during the retrieval process.
+ */
+async function listCollectionNames(client, dbName) {
+    try
+    {
+        const db = client.db(dbName);
+        const collections = await db.listCollections().toArray();
+        const collectionNames = collections.map(col => col.name);
+        console.log(collectionNames);
+    }catch (error) 
+    {
+        console.error("Error listing collections:", error.message);
+    }
+}
+
 async function main() {
     const MONGO_URI = "mongodb+srv://ngsweejie:CS2TMS@cs02taskmanagementsyste.ko3ct.mongodb.net/?retryWrites=true&w=majority&appName=CS02TaskManagementSystem";
     const client = new MongoClient(MONGO_URI);
@@ -38,8 +60,8 @@ async function main() {
         
         await client.connect(); 
         await listDatabases(client);
-        
- 
+        await listCollectionNames(client, "sample_tasks");
+
     } catch (e)
     {
         console.error(e);
--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -4,11 +4,13 @@ import mongoose from "mongoose";
 // https://mongoosejs.com/ READ THE DOCS
 
 const userSchema = new mongoose.Schema({
-    firstName: {type: String, required: True},
-    lastName: String,
-    userName: String,
-    email: String
+    id: {type: Number, required: true },
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    userName: {type: String, required: true},
+    email: {type: String, required: true}
 });
 
 const User = mongoose.model('User', userSchema);
-module.exports = User;
+
+export default User;
