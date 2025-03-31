a1c01a7a4b36566a6453a5312f2f1e947451996a
tda-8818 <ejj_t@outlook.com>
2025-03-31 13:00:00 +1100

    visible settings ui, need to handle backend logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route, useLocation, Navigate, Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
@@ -8,45 +7,6 @@ import Kanban from "./pages/Kanban";
 import Gantt from "./pages/Gantt";
 import Settings from "./pages/Settings";
 import Messages from "./pages/Messages";
-
-function Layout (){
-  const {user} = useSelector((state) => state.auth);
-  const location = useLocation();
-  
-  return user ? (
-    <div className="w-full h-screen flex flex-col md:flex-row">
-      <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
-      {/* <Sidebar/> */}
-      </div>
-
-      <div className="flex-1 overflow-y-auto">
-
-      </div>
-
-    </div>
-  ) : (
-    <Navigate to='/log-in' state={{ from: location }} replace/>
-  )
-}
-
-// function App() {
-//   return (
-//     <main className='w-full min-h-screen bg[f3f446]'>
-//       <Routes>
-//         <Route element={<Layout/>}>
-//           <Route path="/" element={<Navigate to="/home" />} />
-//           <Route path="/home" element={<Home />} />
-//           {/* <Route path="/gantt" element={<Gantt />} />
-//           <Route path="/calendar" element={<Calendar />} />
-//           <Route path="/messages" element={<Messages />} />
-//           <Route path="/settings" element={<Settings />} /> */}
-
-//           <Route path="/log-in" element={<Login />} />
-//         </Route>
-//       </Routes>
-//     </main>
-//   );
-// }
 
 function App() {
   return (
@@ -58,6 +18,10 @@ function App() {
       <Route path='/calendar' element={<Calendar/>}/>
       <Route path='/gantt' element={<Gantt/>}/>
       <Route path='/kanban' element={<Kanban/>} />
+      <Route path='/messages' element={<Messages/>}/>
+      <Route path='/settings' element={<Settings/>}/>
+
+      <Route path='*' element={<Navigate to='/login'/>}/> {/* Redirect to login page if route not found */} 
     </Routes>
   
   );
