15a38f7ec36b037dc57e7528e704c62ddb58079b
tda-8818 <ejj_t@outlook.com>
2025-04-09 14:51:12 +1000

    added .env configurations for security of our app

--- server/src/index.js
+++ server/src/index.js
@@ -6,40 +6,39 @@
 import express, {json} from 'express'; // Express.js used for creating the server
 import cors from 'cors'; // CORS is a Connect/Express middleware for handling cross-origin requests.
 import userRoutes from './routes/userRoutes.js';
-//import projectRoutes from './routes/projectRoutes.js';
-//import taskRoutes from './routes/taskRoutes.js';
-import { connect } from 'mongoose'; // Mongoose used for connecting to MongoDB
-import path from 'path'; // Path module provides utilities for working with file and directory paths
-import { fileURLToPath } from 'url'; // fileURLToPath is used to convert a URL to a file path
+import mongoose from 'mongoose'; // Mongoose used for connecting to MongoDB
+import dotenv from 'dotenv'; 
+
+dotenv.config(); // Load environment variables from .env file
 
 // Create an Express server
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 // Middleware set up:
 app.use(cors()); 
 app.use(json()); // parse incoming JSON requests
+const mongoURI = process.env.MONGO_URI; // MongoDB connection URI
 
 // Connect to the MongoDB database
-const MONGO_URI = "mongodb+srv://ngsweejie:CS2TMS@cs02taskmanagementsyste.ko3ct.mongodb.net/users?retryWrites=true&w=majority&appName=CS02TaskManagementSystem";
-connect(MONGO_URI);
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,})
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);});
 
 
 // Import routes
 app.use('/api/users', userRoutes);
-// app.use('/api/tasks', taskRoutes);
-// app.use('/api/projects', projectRoutes);
 
-// Get the current directory using import.meta.url
-const fileName = fileURLToPath(import.meta.url);
-const dirName = path.dirname(fileName);
-
-// Serve static files from the React app
-app.use(express.static(path.join(dirName, '../client/dist')));
-// Serve the index.html file for all other routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(dirName, '../client/dist/index.html'));
-});
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
 
 // Start the Express server
 app.listen(port, () => {
