1483c9dd6cce37001700dd49011a6044125d4d1a
Swee Jie Ng <ngsweejie@gmail.com>
2025-05-09 10:30:13 +1000

    Merged and fixed navigation errors

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,7 +3,11 @@ import logo from '../assets/logo.png';
 import { RxHome } from "react-icons/rx";
 import { useNavigate } from 'react-router-dom';
 import { useGetCurrentUserProjectsQuery } from '../redux/slices/projectSlice';
-
+import {
+  useGetProjectByIdQuery,
+  useGetProjectUsersQuery,
+  useUpdateProjectUsersMutation
+} from '../redux/slices/projectSlice';
 const navItems = [
   { label: "Home", icon: <RxHome />, href: "/Home" }
 ];
