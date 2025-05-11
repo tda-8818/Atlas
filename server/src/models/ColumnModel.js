15f5fe2bfc06596c6f93a11d5f0a5c98df441fb1
Swee Jie Ng <ngsweejie@gmail.com>
2025-05-11 19:45:34 +1000

    Columns now render into the kanban board from the DB

--- server/src/models/ColumnModel.js
+++ server/src/models/ColumnModel.js
@@ -7,7 +7,7 @@ const columnSchema = new mongoose.Schema({
     title: {type: String, required: true},
     projectId: {type: mongoose.Schema.Types.ObjectId, ref: 'project', required: true},
     tasks: [{type: mongoose.Schema.Types.ObjectId, ref:'task',  default: []}],
-    position: { type: Number, required: true}, // Position is the order of the column from left to right
+    index: { type: Number, required: true}, // index is the order of the column from left to right
     isDefault: {type: Boolean, default: false}, // Columns created should not set this value.  
 }, {timestamps: true});
 
