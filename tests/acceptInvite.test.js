cf3b977a241b3df74e8d9e5ac60a75a5c41d5b25
Swee Jie Ng <ngsweejie@gmail.com>
2025-05-23 14:43:24 +1000

    Creating Tests

---
+++ tests/acceptInvite.test.js
@@ -0,0 +1,4 @@
+import { userAcceptInvite } from "../server/src/controllers/projectController";
+import httpMock from 'node-mocks-http';
+import { Project } from "../server/src/models/ProjectModel";
+import { Notification} from  "../server/src/models/notificationModel";
